73f4cf00ecbc17967dfa740a236f9e642ec9b355
baixianger <58243749+baixianger@users.noreply.github.com>
2025-05-04 12:47:25 +0200

    add simple import sort

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,24 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  // 加载 Next.js 的旧版配置（通过 FlatCompat 转换）
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  
+  // 添加 simple-import-sort 插件
+  {
+    plugins: {
+      "simple-import-sort": [
+        simpleImportSort,
+        {
+          "groups": [["^\\u0000", "^@?\\w", "^[^.]", "^\\."]]
+        }
+      ]
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
 ];
-
+ 
 export default eslintConfig;
