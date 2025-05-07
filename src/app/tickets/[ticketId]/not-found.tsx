46175033fd5a3590d270530c1853d107ca256784
baixianger <58243749+baixianger@users.noreply.github.com>
2025-05-07 14:17:06 +0200

    realize a self-customized not found page though next.js

---
+++ src/app/tickets/[ticketId]/not-found.tsx
@@ -0,0 +1,20 @@
+import { LucideTicketX } from "lucide-react";
+import Link from "next/link";
+import { Placeholder } from "@/components/placeholder";
+import { Button } from "@/components/ui/button";
+import { ticketsPath } from "@/paths";
+
+export default function NotFound() {
+  return (
+    <Placeholder
+      label="Ticket not found"
+      icon={<LucideTicketX/>}
+      button={
+        <Button asChild variant="outline">
+          <Link href={ticketsPath()}>Go to Tickets</Link>
+        </Button>
+      }
+    />
+  );
+}
+      
