f95dca079a0a3e3ff9e462d3ee50314695b2180d
baixianger <58243749+baixianger@users.noreply.github.com>
2025-05-11 00:05:29 +0200

    修改直接类型引用，变成类型推断

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -1,4 +1,3 @@
-import { Ticket } from "@prisma/client";
 import clsx from "clsx";
 import { LucideSquareArrowOutUpRight } from "lucide-react";
 import Link from "next/link";
@@ -10,15 +9,30 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { TICKET_ICONS } from "@/features/ticket/constants";
+import { getTicket } from "@/features/ticket/queries/get-ticket";
+import { getTickets } from "@/features/ticket/queries/get-tickets";
 import { ticketPath } from "@/paths";
 
+// import { Ticket } from "@prisma/client";
+// type TicketItemProps = {
+//   ticket: Ticket;
+//   isDetail?: boolean;
+// };
+// 更好的方式是用类型推导来获取ticket的类型
 type TicketItemProps = {
-  ticket: Ticket;
+  ticket: 
+  | Awaited<ReturnType<typeof getTickets>>[number]
+  | Awaited<ReturnType<typeof getTicket>>;
   isDetail?: boolean;
 };
 
 export const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
-  console.log("Where am I displayed? (TicketItem)");
+
+  // check null or the certain type wouldn't have the id property
+  // if (!ticket) {
+  //   return null;
+  // }
+  // 如果不想要check步骤，可以修改getTicket里的查询函数为findUniqueOrThrow，这样绝不会返回null，就不会出现ticket.id的情况
 
   const detailButton = (
     <Button variant="outline" size="icon" asChild>
--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,10 +1,11 @@
 import { Ticket } from "@prisma/client";
 import { prisma } from "@/lib/prisma";
 
-export const getTicket = async (ticketId: string): Promise<Ticket | null> => {
+export const getTicket = async (ticketId: string): Promise<Ticket> => {
   // two ways to get the ticket findUnique and findUniqueOrThrow
-  // findUniqueOrThrow will throw an error if the ticket is not found
-  return await prisma.ticket.findUnique({
+  // findUniqueOrThrow will throw an error if the ticket is not found, Promise<Ticket>
+  // findUnique will return null if the ticket is not found, Promise<Ticket | null>
+  return await prisma.ticket.findUniqueOrThrow({
     where: {
       id: ticketId,
     },
