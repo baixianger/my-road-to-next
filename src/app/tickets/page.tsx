6f14bfbbb59aa29bb3de4d2e8c9c0ece968aecda
baixianger <58243749+baixianger@users.noreply.github.com>
2025-05-13 00:40:16 +0200

    Incremental Static Regeneration (ISR) 不修改页面为动态，但保持更新

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -11,7 +11,13 @@ import { TicketList } from "@/features/ticket/components/ticket-list";
 // 如果是对于博客这种新闻类的页面，影响不大；但是对于数据变化频繁的页面，比如票务系统，
 // 可能会影响到数据的实时性，所以可以使用动态路由，
 // 解决方案是 https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
-export const dynamic = 'force-dynamic'; // 这个页面会被编译成一个动态页面，在build后会显示（ƒ）字样
+// export const dynamic = 'force-dynamic'; // 这个页面会被编译成一个动态页面，在build后会显示（ƒ）字样
+
+// 另外一个方案是保留静态页面，但是设置一个缓存时间。Incremental Static Regeneration (ISR)
+// https://nextjs.org/docs/app/building-your-application/data-fetching/incremental-static-regeneration
+// https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#revalidate
+// 时间调成0秒后，实际等效一个动态页面。
+export const revalidate = 60
 
 const TicketsPage = () => {
   return (
