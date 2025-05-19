dc27676b23958fc6dc7db86921ad656029fb1dc0
baixianger <58243749+baixianger@users.noreply.github.com>
2025-05-19 22:36:07 +0200

    103. useActionState

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -6,7 +6,10 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 
-export const upsertTicket = async (formData: FormData) => {
+export const upsertTicket = async (
+  _actionState: {message: string},
+  formData: FormData
+) => {
   // 这里的formData是一个FormData对象，包含了表单提交的数据
   // 你可以使用formData.get('title')来获取表单字段的值
   const data = {
@@ -29,4 +32,8 @@ export const upsertTicket = async (formData: FormData) => {
   if (id) {
     redirect(ticketsPath());
   }
+
+  return {
+    message: "Ticket created successfully",
+  };
 };
--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -6,17 +6,27 @@ import { Textarea } from "@/components/ui/textarea"
 import { getTicket } from "../queries/get-ticket"
 import { upsertTicket } from "../actions/upsert-ticket"
 import { SubmitButton } from "@/components/submit-button"
-
+import { useActionState } from "react"
 
 type TicketUpsertFormProps = {
   ticket?: Awaited<ReturnType<typeof getTicket>>;
 }
 
-const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
 
+const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
+  // useActionState takes a function that returns a promise and an initial state, 
+  // the function will be called when the form is submitted, and the state will be updated with the result of the function
+  // binding the state to the form action, the state will be updated when the form is submitted.
+  // So the state can wrap a lot of information about the result of the action.
+  const [actionState, action] = useActionState(
+    upsertTicket,
+    {
+      message: "",
+    }
+  )
   return (
 
-    <form action={upsertTicket} className="flex flex-col gap-y-2">
+    <form action={action} className="flex flex-col gap-y-2">
       <Input type="hidden" name="id" defaultValue={ticket?.id} />
 
       <Label htmlFor="title">Title</Label>
@@ -26,6 +36,8 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
       <Textarea id="content" name="content" defaultValue={ticket?.content} />
 
       <SubmitButton label={ticket ? "Update" : "Create"} />
+
+      {actionState.message}
     </form>
   )
 
