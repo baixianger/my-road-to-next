7fd69571d8ce85180469cf9844194da27b4d6d8b
baixianger <58243749+baixianger@users.noreply.github.com>
2025-05-19 21:56:45 +0200

    102. useFormStatus

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -1,13 +1,12 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 import { getTicket } from "../queries/get-ticket"
 import { upsertTicket } from "../actions/upsert-ticket"
-import { useTransition } from "react"
-import { LucideLoaderCircle } from "lucide-react"
+import { SubmitButton } from "@/components/submit-button"
+
 
 type TicketUpsertFormProps = {
   ticket?: Awaited<ReturnType<typeof getTicket>>;
@@ -15,16 +14,9 @@ type TicketUpsertFormProps = {
 
 const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
 
-  const [isPending, startTransition] = useTransition()
-  const upsertTicketAction = async ( formData: FormData) => {
-    startTransition(async () => {
-      await upsertTicket(formData); // 如果有bind数据，这里也要bind
-    });
-  }
-
   return (
 
-    <form action={upsertTicketAction} className="flex flex-col gap-y-2">
+    <form action={upsertTicket} className="flex flex-col gap-y-2">
       <Input type="hidden" name="id" defaultValue={ticket?.id} />
 
       <Label htmlFor="title">Title</Label>
@@ -33,10 +25,7 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
       <Label htmlFor="content">Content</Label>  
       <Textarea id="content" name="content" defaultValue={ticket?.content} />
 
-      <Button type="submit">
-        {isPending && <LucideLoaderCircle className="mr-2 h-4 w-4 animate-spin" />}
-        {ticket ? "Edit" : "Create"}
-        </Button>
+      <SubmitButton label={ticket ? "Update" : "Create"} />
     </form>
   )
 
