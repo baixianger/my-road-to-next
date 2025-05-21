0966419e1d3e7e1af89817039bbc7161d0f7e02d
baixianger <58243749+baixianger@users.noreply.github.com>
2025-05-21 23:48:28 +0200

    115. Read Cookie

--- src/components/redirect-toast.tsx
+++ src/components/redirect-toast.tsx
@@ -5,11 +5,17 @@ import { toast } from "sonner";
 import { getCookieByKey, deleteCookieByKey } from "@/action/cookies";
 
 export const RedirectToast = () => {
+  /** 
+   * 这里会打印两次message，是因为useEffect在开发模式下会被调用两次，但是在生产模式下只会调用一次。
+   * https://react.dev/reference/react/StrictMode
+   * 如果不想在开发模式下打印两次，可以使用strictMode
+   * 用<StrictMode>包裹你的应用程序，这样在开发模式下，useEffect只会被调用一次。
+  */
   useEffect(() => {
     const checkToast = async () => {
       try {
         const toastMessage = await getCookieByKey("toast");
-        console.log("toastMessage", toastMessage);
+        console.log("toastMessage", toastMessage); 
         if (toastMessage) {
           toast.success(toastMessage);
           deleteCookieByKey("toast");
