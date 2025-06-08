b4780855b42ef9809de6c704be501a7fea96649b
baixianger <58243749+baixianger@users.noreply.github.com>
2025-06-08 23:58:41 +0200

    157. Authenticated Sidebar and Sidebar animation

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -3,8 +3,12 @@ import { useState } from "react";
 import { cn } from "@/lib/utils";
 import { sidebarNav } from "./constants";
 import { SidebarItem } from "./sidebar-item";
+import { useAuth } from "@/features/auth/hooks/use-auth";
 
 const Sidebar = () => {
+
+  const {user, isFetched } = useAuth();
+
   const [isTransition, setIsTransition] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
@@ -14,9 +18,14 @@ const Sidebar = () => {
     setTimeout(() => setIsTransition(false), 300);
   };
 
+  if (!user || !isFetched) {
+    return <div className="w-[55px] bg-secondary" />;
+  }
+
   return (
     <nav
       className={cn(
+        "animate-sidebar-from-left",
         "h-screen border-r pt-24",
         isTransition && "duration-200",
         isOpen ? "md:w-60 w-[55px]" : "w-[55px]"
