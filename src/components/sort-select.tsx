35378002793a9a552f2b3e22cfb04f0e097e875f
baixianger <58243749+baixianger@users.noreply.github.com>
2025-06-11 10:08:01 +0200

    171. Composite Key

--- src/components/sort-select.tsx
+++ src/components/sort-select.tsx
@@ -24,21 +24,25 @@ type SortSelectProps = {
 export const SortSelect = ({ options }: SortSelectProps) => {
   const [sort, setSort] = useQueryStates(sortParser, sorthOptions);
 
-  const handleSort = (sortKey: string) => {
-    const sortValue = options.find(
-      (option) => option.sortKey === sort.sortKey
-    )?.sortValue;
+  const handleSort = (compositeKey: string) => {
+    const [sortKey, sortValue] = compositeKey.split("_");
     setSort({ sortKey, sortValue });
   };
 
   return (
-    <Select defaultValue={sort.sortKey} onValueChange={handleSort}>
+    <Select
+      defaultValue={sort.sortKey + "_" + sort.sortValue}
+      onValueChange={handleSort}
+    >
       <SelectTrigger>
         <SelectValue />
       </SelectTrigger>
       <SelectContent>
         {options.map((option) => (
-          <SelectItem key={option.sortKey} value={option.sortKey}>
+          <SelectItem
+            key={option.sortKey + option.sortValue}
+            value={option.sortKey + "_" + option.sortValue}
+          >
             {option.label}
           </SelectItem>
         ))}
